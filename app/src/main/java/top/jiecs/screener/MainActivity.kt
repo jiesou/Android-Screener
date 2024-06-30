d2f79c1b4ed4fce64732906fe7222fb9e01245d9
jiesou <jiechushou@gmail.com>
2024-06-30 19:41:32 +0800

    update app icon color (for new SplashScreen)

--- app/src/main/java/top/jiecs/screener/MainActivity.kt
+++ app/src/main/java/top/jiecs/screener/MainActivity.kt
@@ -29,6 +29,7 @@ class MainActivity : AppCompatActivity(),
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
+        window.isNavigationBarContrastEnforced = false
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
